320eb86a481d4782a5e7901a1caf660f6b88ed8d
Robert Patton <robpatton04@gmail.com>
2025-08-15 14:22:49 +0100

    deploy/path fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import { WebSocketProvider } from './context/WebSocketContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ToastProvider>
         <WebSocketProvider>
           <App />
