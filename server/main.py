866179602ab6519bdc750552dd55351d4358dded
rpattn <robpatton04@gmail.com>
2026-02-15 17:02:42 +0100

    add nw port

--- server/main.py
+++ server/main.py
@@ -20,6 +20,10 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "http://localhost:8101",
+        "http://127.0.0.1:8101",
+        "http://localhost:8100",
+        "http://127.0.0.1:8100",
         "https://rpattn.github.io/WOMBAT_ext/",
         "https://rpattn.github.io"
     ],
